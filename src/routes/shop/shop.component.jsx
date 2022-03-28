6e7ace8a120ba986fb073446fe2347c8b7826c08
Lawrence Dass <lawrence.dass@publicissapient.com>
2022-03-28 18:49:34 +0530

    add the data to firestore and create context for categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,30 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
+import { useContext } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
 
-import Product from "../../components/product/product.component";
+import Product from '../../components/product/product.component';
 
 import './shop.styles.scss';
 
-
 const Shop = () => {
-    const products = useContext(ProductsContext);
-
-    return (
-    <div className="products-container">
-        {products?.map((product) => {
-            return (<Product key={product.id} product={product} />)
-        })}
-    </div>
-    )
-}
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log({ categoriesMap });
+  //   return <h2>Shop component</h2>;
+  return (
+    <>
+      {Object.keys(categoriesMap).map((title) => {
+        return (
+          <>
+            <h2>{title}</h2>
+            <div className="products-container">
+              {categoriesMap[title]?.map((product) => {
+                return <Product key={product.id} product={product} />;
+              })}
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default Shop;
